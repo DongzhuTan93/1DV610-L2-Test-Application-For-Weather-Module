bfcbbd389e045f7d2ec4702d2b5902e6ee13116a
Dongzhu Tan <dt222ha@student.lnu.se>
2023-11-23 14:00:14 +0100

    Add javascript application dependencies

---
+++ src/app.js
@@ -0,0 +1,19 @@
+/**
+ * Where the application starts.
+ *
+ * @author Dongzhu Tan <dt222ha@student.lnu.se>
+ */
+
+/**
+ * The main function of the application.
+ *
+ */
+const main = async () => {
+  try {
+    // ..
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+main()
